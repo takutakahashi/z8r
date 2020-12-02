dc319ac0a27d4e9ddf6b4f0752475500e8127617
takutaka <takutaka@pepabo.com>
2020-12-02 23:06:37 +0900

    exec_snapshot.py to oneshot script

--- entrypoint_daemon.sh
+++ entrypoint_daemon.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 /usr/sbin/sshd
-python3 -u /exec_snapshot.py &
 while true; do
+  python3 -u /exec_snapshot.py
   curl replicator/id_rsa.pub 1>/root/.ssh/authorized_keys 2>/dev/null
   sleep 10
 done
