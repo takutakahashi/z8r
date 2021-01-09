6fdfb0480ed4a53e1393bfa0b65d477bf23883bc
Takuya TAKAHASHI <taku.takahashi120@gmail.com>
2021-01-10 02:20:52 +0900

    mod repl

--- replication.sh
+++ replication.sh
@@ -22,16 +22,16 @@ if [[ "$DSTHOST" = "" ]]; then
   exit 1
 fi
 
-src_last_snapshot=`ssh $SRC zfs list -t snapshot $SRCPOOL |grep -v NAME |awk '{print $1}' |sort |tail -1`
-dst_last_snapshot=`ssh $DST zfs list -t snapshot $DSTPOOL |grep -v NAME |awk '{print $1}' |sort |tail -1`
-src_first_snapshot=`ssh $SRC zfs list -t snapshot $SRCPOOL |grep -v NAME |awk '{print $1}' |sort |head -1`
-dst_first_snapshot=`ssh $DST zfs list -t snapshot $DSTPOOL |grep -v NAME |awk '{print $1}' |sort |head -1`
+src_last_snapshot=`ssh $SRCHOST zfs list -t snapshot $SRCPOOL |grep -v NAME |awk '{print $1}' |sort |tail -1`
+dst_last_snapshot=`ssh $DSTHOST zfs list -t snapshot $DSTPOOL |grep -v NAME |awk '{print $1}' |sort |tail -1`
+src_first_snapshot=`ssh $SRCHOST zfs list -t snapshot $SRCPOOL |grep -v NAME |awk '{print $1}' |sort |head -1`
+dst_first_snapshot=`ssh $DSTHOST zfs list -t snapshot $DSTPOOL |grep -v NAME |awk '{print $1}' |sort |head -1`
 if [[ "$dst_last_snapshot" = "$src_last_snapshot" ]]; then
     exit 0
 fi
 if [[ "$dst_first_snapshot" = "" ]]; then
     ssh $SRCHOST zfs send $src_first_snapshot |ssh $DSTHOST zfs recv $DSTPOOL -F
 elif [[ `echo $dst_first_snapshot |awk -F'@' '{print $2}'` -lt `echo $src_first_snapshot |awk -F'@' '{print $2}'` ]]; then
-    ssh $DST zfs destroy $dst_first_snapshot
+    ssh $DSTHOST zfs destroy $dst_first_snapshot
 fi
 ssh $SRCHOST zfs send -I $src_first_snapshot $src_last_snapshot |ssh $DSTHOST zfs recv $DSTPOOL
