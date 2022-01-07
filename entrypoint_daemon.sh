687fe08fa28e612fff571cdc53806a9b5e8ae010
Takuya TAKAHASHI <taku.takahashi120@gmail.com>
2022-01-07 23:28:19 +0900

    mod

--- entrypoint_daemon.sh
+++ entrypoint_daemon.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-kubectl get pod -o wide -n z8r |grep node-daemon|awk '{print $6," ",$7}' >> /etc/hosts
 /usr/sbin/sshd
 if [[ -e "/root/.ssh/authorized_keys" ]]; then
-  sleep infinity
+  while true; do
+    sleep 30
+    kubectl get pod -o wide -n z8r |grep node-daemon|awk '{print $6," ",$7}' > /etc/hosts
+  done
 else
   while true; do
     curl replicator/id_rsa.pub 1>/root/.ssh/authorized_keys 2>/dev/null
